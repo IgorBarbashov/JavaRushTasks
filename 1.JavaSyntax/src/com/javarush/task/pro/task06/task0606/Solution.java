cc10e53143e40a2d8234b4f0da733d95868f5c93
Igor Barbashov <ibarbashov@freeconferencecall.com>
2022-04-29 00:18:13 +0300

    task06.task0606

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,29 @@
+package com.javarush.task.pro.task06.task0606;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){}
+
+    public static void universalMethod(int i1){}
+
+    public static void universalMethod(int i1, int i2){}
+
+    public static void universalMethod(int i1, int i2, int i3){}
+
+    public static void universalMethod(int i1, int i2, int i3, int i4){}
+
+    public static void universalMethod(int i1, int i2, int i3, int i4, int i5){}
+
+    public static void universalMethod(String i1){}
+
+    public static void universalMethod(String i1, String i2){}
+
+    public static void universalMethod(String i1, String i2, String i3){}
+
+    public static void universalMethod(String i1, String i2, String i3, String i4){}
+
+}
