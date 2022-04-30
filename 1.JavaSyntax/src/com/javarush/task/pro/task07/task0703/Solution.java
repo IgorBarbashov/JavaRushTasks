6ed824fc199230d68f47f40d8c5d3fd7127b2d65
Igor Barbashov <bis200705@gmail.com>
2022-04-30 22:32:20 +0300

    task07.task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,8 @@
+package com.javarush.task.pro.task07.task0703;
+
+public class Solution {
+    byte b = Byte.MIN_VALUE;
+    short s = Short.MIN_VALUE;
+    int i = Integer.MIN_VALUE;
+    long l = Long.MIN_VALUE;
+}
