19962f51659351c6af3c2ae8dc7351bf034e75e3
Igor Barbashov <bis200705@gmail.com>
2022-04-30 22:17:21 +0300

    task07.task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0701;
+
+public class Solution {
+    byte b;
+    short s;
+    int i;
+    long l;
+    float f;
+    double d;
+    boolean bo;
+    char c;
+}
