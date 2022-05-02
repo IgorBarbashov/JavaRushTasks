a85290ee9d5a9ddbb43edff6ae04683ed9a34653
Igor Barbashov <bis200705@gmail.com>
2022-05-02 11:53:57 +0300

    task08.task0803

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task08/task0803/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task08.task0803;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[] intArray = getArrayOfTenElements();
+        System.out.println(min(intArray));
+    }
+
+    public static int min(int[] ints) {
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < ints.length; i++) {
+            min = Math.min(min, ints[i]);
+        }
+        return min;
+    }
+
+    public static int[] getArrayOfTenElements() {
+        int[] array = new int[10];
+        Scanner in = new Scanner(System.in);
+
+        for (int i = 0; i < 10; i++) {
+            array[i] = in.nextInt();
+        }
+
+        return array;
+    }
+}
