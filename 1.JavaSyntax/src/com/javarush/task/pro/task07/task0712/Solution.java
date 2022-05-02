a7f15e7fe62dfb34c8cba86f441f8834d48253a5
Igor Barbashov <bis200705@gmail.com>
2022-05-02 11:12:46 +0300

    task07.task0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task07.task0712;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String str1 = new String("str1");
+        String str2 = new String("str2");
+        String str3 = new String("str3");
+        String str4 = new String("str4");
+        String str5 = new String("str5");
+        String str6 = new String("str6");
+        String str7 = new String("str7");
+        String str8 = new String("str8");
+        String str9 = new String("str9");
+        String str10 = new String("str10");
+
+        int[] i1 = new int[]{1};
+        int[] i2 = new int[]{2};
+        int[] i3 = new int[]{3};
+        int[] i4 = new int[]{4};
+        int[] i5 = new int[]{5};
+
+        Scanner scanner1 = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+    }
+}
