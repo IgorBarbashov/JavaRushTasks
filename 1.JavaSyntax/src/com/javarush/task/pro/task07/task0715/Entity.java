3205b269e51c1c3e31e9086ba0ecb5b626b084ac
Igor Barbashov <bis200705@gmail.com>
2022-05-02 11:34:03 +0300

    task07.task0715

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0715/Entity.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task07.task0715;
+
+public class Entity {
+    public void move() {
+        System.out.println("Я передвигаюсь.");
+    }
+
+    public void eat() {
+        System.out.println("Я ем.");
+    }
+}
